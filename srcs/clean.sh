70c113e60a0c90384767accce0f4bec206c4bf05
Mo <mel-kada@student.42lyon.fr>
2022-07-28 11:33:28 +0200

    k

--- srcs/clean.sh
+++ srcs/clean.sh
@@ -1,4 +1,4 @@
-docker-compose --project-directory=./srcs down
+docker-compose -f ./srcs/docker-compose.yml down
 docker volume rm $(docker volume ls -q)
 docker system prune -f
 rm -rf /home/mel-kada/data/mariadb/* /home/mel-kada/data/wordpress/*
--- srcs/requirements/wordpress/conf/start.sh
+++ srcs/requirements/wordpress/conf/start.sh
@@ -7,7 +7,7 @@ while ! nc -vz mariadb 3306; do sleep 1; done
 sleep 5
 
 wp core download --allow-root
-wp core install --url="https://localhost" --title="ENFIN/ $1" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
+wp core install --url="https://$DOMAIN_NAME" --title="ENFIN/ $1" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
 wp user create $USER_NAME $USER_EMAIL --user_pass=$USER_PASS --role=editor --allow-root
 
 #wp plugin install redis-cache --activate --allow-root
